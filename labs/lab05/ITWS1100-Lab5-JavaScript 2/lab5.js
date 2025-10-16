8ce056c653736d254275e839bd303c67168d6e4b
nanamichrysler <chrysn@rpi.edu>
2025-10-16 15:34:18 -0400

    in class
    in class practice with JavaScript

---
+++ labs/lab05/ITWS1100-Lab5-JavaScript 2/lab5.js
@@ -0,0 +1,14 @@
+/* Lab 5 JavaScript File 
+   Place variables and functions in this file */
+
+function validate(formObj) {
+   // put your validation code here
+   // it will be a series of if statements
+
+   if (formObj.firstName.value == "") {
+      alert("You must enter a first name");
+      formObj.firstName.focus();
+      return false;
+   }
+   return true;
+}
