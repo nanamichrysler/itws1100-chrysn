819e10cdd5ffabfcfd499e0e341b5b0db940c1f1
nanamichrysler <chrysn@rpi.edu>
2025-11-28 22:38:46 -0500

    Update movies.php

--- labs/lab09/inclassexample/movies.php
+++ labs/lab09/inclassexample/movies.php
@@ -1,123 +1,51 @@
-<?php 
-  include('includes/init.inc.php'); 
-  include('includes/functions.inc.php');
-?>
-<title>PHP &amp; MySQL - ITWS</title>
-
-<?php include('includes/head.inc.php'); ?>
-
-<h1>PHP &amp; MySQL</h1>
-
-
-<?php include('includes/menubody.inc.php'); ?>
-
 <?php
+// movies.php - debug-friendly version
 
-$dbOk = false;
+// Show all errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-@$db = new mysqli($GLOBALS['DB_HOST'], $GLOBALS['DB_USERNAME'], $GLOBALS['DB_PASSWORD'], $GLOBALS['DB_NAME']);
+// Include config and functions if you want
+include('includes/config.inc.php'); // make sure this has correct DB credentials
+include('includes/functions.inc.php');
 
+// Connect to the database
+$db = new mysqli($GLOBALS['DB_HOST'], $GLOBALS['DB_USERNAME'], $GLOBALS['DB_PASSWORD'], $GLOBALS['DB_NAME']);
+
+// Check connection
 if ($db->connect_error) {
-   echo '<div class="messages">Could not connect to the database. Error: ';
-   echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+    die("<p>Database connection failed: " . $db->connect_error . "</p>");
+}
+
+// Optional: check what database we are connected to
+echo "<p>Connected to database: " . $GLOBALS['DB_NAME'] . "</p>";
+
+// Fetch movies
+$query = "SELECT * FROM movies ORDER BY year";
+$result = $db->query($query);
+
+if (!$result) {
+    die("<p>Query failed: " . $db->error . "</p>");
+}
+
+// Display movies
+echo "<h1>Movies Table</h1>";
+
+if ($result->num_rows > 0) {
+    echo "<table border='1' cellpadding='5' cellspacing='0'>";
+    echo "<tr><th>Movie ID</th><th>Title</th><th>Year</th></tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($row['movieid']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['title']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['year']) . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
 } else {
-   $dbOk = true;
+    echo "<p>No movies found in the table.</p>";
 }
 
-$havePost = isset($_POST["save"]);
-
-$errors = '';
-if ($havePost) {
-
-   $title = htmlspecialchars(trim($_POST["title"]));
-   $year = htmlspecialchars(trim($_POST["year"]));
-
-   $focusId = '';
-
-   if ($title == '') {
-      $errors .= '<li>Title may not be blank</li>';
-      if ($focusId == '') $focusId = '#title';
-   }
-   if ($year == '') {
-      $errors .= '<li>Year may not be blank</li>';
-      if ($focusId == '') $focusId = '#year';
-   }
-
-   if ($errors != '') {
-      echo '<div class="messages"><h4>Please correct the following errors:</h4><ul>';
-      echo $errors;
-      echo '</ul></div>';
-      echo '<script type="text/javascript">';
-      echo '$(document).ready(function() { $("' . $focusId . '").focus(); });';
-      echo '</script>';
-   } else {
-      if ($dbOk) {
-
-         $title = trim($_POST["title"]);
-         $year = trim($_POST["year"]);
-
-         // FIXED insert query
-         $insQuery = "INSERT INTO movies (title, year) VALUES (?, ?)";
-         $statement = $db->prepare($insQuery);
-
-         // Only 2 params, both strings
-         $statement->bind_param("ss", $title, $year);
-
-         $statement->execute();
-
-         echo '<div class="messages"><h4>Success: ' . $statement->affected_rows . ' movie added to database.</h4>';
-         echo $title . ' released in ' . $year . '</div>';
-
-         $statement->close();
-      }
-   }
-}
+// Close connection
+$db->close();
 ?>
-
-<h3>Add Movie</h3>
-<form id="addForm" name="addForm" action="movies.php" method="post">
-   <fieldset>
-      <div class="formData">
-
-         <label class="field" for="title">Title:</label>
-         <div class="value"><input type="text" size="60" 
-            value="<?php if ($havePost && $errors != '') echo $title; ?>" 
-            name="title" id="title" /></div>
-
-         <label class="field" for="year">Year:</label>
-         <div class="value"><input type="text" size="60" 
-            value="<?php if ($havePost && $errors != '') echo $year; ?>" 
-            name="year" id="year" /></div>
-
-         <input type="submit" name="save" value="Save Movie" />
-
-      </div>
-   </fieldset>
-</form>
-
-<h3>Movies</h3>
-<table id="movieTable">
-<?php
-if ($dbOk) {
-
-   $query = "SELECT * FROM movies ORDER BY year";
-   $result = $db->query($query);
-
-   echo '<tr><th>Title</th><th>Year</th></tr>';
-
-   while ($record = $result->fetch_assoc()) {
-
-      echo '<tr id="movie-' . $record['movieid'] . '"><td>';
-      echo htmlspecialchars($record['title']);
-      echo '</td><td>';
-      echo htmlspecialchars($record['year']);
-      echo '</td></tr>';
-   }
-
-   $result->free();
-   $db->close();
-}
-?>
-</table>
-
-<?php include('includes/foot.inc.php'); ?>
