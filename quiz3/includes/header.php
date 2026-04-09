baf45c0167affd5be13aa89fbe2531c2ec9c09b0
nanamichrysler <chrysn@rpi.edu>
2026-04-09 17:54:36 -0400

    header footer

--- quiz3/includes/header.php
+++ quiz3/includes/header.php
@@ -1,14 +1,11 @@
 <html>
   <head>
-    <title>
-      Nanami Chrysler's ITWS 1100 
-    </title>
-    <link rel="stylesheet" href="./resources/mySite.css">
+    <title>Nanami Chrysler's ITWS 1100</title>
+    <link rel="stylesheet" href="/resources/mySite.css">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="js/comment.js"></script>
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
   </head>
   <body>
     <div id="header"></div>
--- resources/header.js
+++ resources/header.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const header = document.getElementById('header');
-
   header.innerHTML = `
     <div class="banner">
       <a href="/home.php"><h1>Nanami Chrysler</h1></a>
       <div class="nav-links">
-        <a href="/aboutMe/aboutme.html">About Me</a>
-        <a href="/classes/classes.html">Classes</a>
-        <a href="/projects/project.html">Projects</a>
-        <a href="/contactMe/contactme.html">Contact Me</a>
+        <a href="/home.php" class="button cta">Home</a>
+        <a href="/aboutMe/aboutme.html" class="button cta">About Me</a>
+        <a href="/classes/classes.html" class="button cta">Classes</a>
+        <a href="/projects/project.html" class="button cta">Projects</a>
+        <a href="/contactMe/contactme.html" class="button cta">Contact Me</a>
       </div>
     </div>
   `;
